a454a587b52a3241c7f7c6519b3148c88269e39e
Shubhamzope1 <7666567671+Shubhamzope1@users.noreply.github.com>
2021-10-18 12:57:07 +0530

    done

--- blogapp/src/components/Header.js
+++ blogapp/src/components/Header.js
@@ -18,14 +18,14 @@ class Header extends React.Component {
      <div>
   <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand ><NavLink className="navigaters" to="/contact">Be Motivated</NavLink></Navbar.Brand>
+  <Navbar.Brand ><NavLink className="navigaters" to="/">Be Motivated</NavLink></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
       <Nav.Link href="/Digital"><NavLink className="navigaters" to="/Digital">Digital</NavLink></Nav.Link>
-      <Nav.Link href="/profs"><NavLink className="navigaters" to="/professional">professional</NavLink></Nav.Link>
+      <Nav.Link href="/profs"><NavLink className="navigaters" to="/profs">professional</NavLink></Nav.Link>
       <NavDropdown title="More" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="/report"><NavLink className="navigater" to="/feeds">FeedBack</NavLink></NavDropdown.Item>
+        <NavDropdown.Item href="/report"><NavLink className="navigater" to="/report">FeedBack</NavLink></NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2"><NavLink className="navigater" to="/contact">Contact-Us</NavLink></NavDropdown.Item>
       </NavDropdown>
     </Nav>
